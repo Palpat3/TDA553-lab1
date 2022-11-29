244a8f07ea1d87240314ec4b7e0273dcd85c5129
maxdreifeldt <maxdreifeldt02@gmail.com>
2022-11-29 17:22:31 +0100

    cartransporter created, old test not working

--- src/set/CarTransporter.java
+++ src/set/CarTransporter.java
@@ -9,6 +9,7 @@ public class CarTransporter extends Truck {
     private boolean rampUp = true;
     private final int maxCarsOnTruck = 5;
     private ArrayList<Car> CarsOnTruck = new ArrayList<>(maxCarsOnTruck);
+    private final double minDistanceToDriveOnTruck = 5;
 
     public CarTransporter(){
         super(2, 90, Color.green, "Car Transporter", 0, 0);
@@ -28,22 +29,30 @@ public class CarTransporter extends Truck {
         
     }
 
+    public boolean isRampUp() {
+        return rampUp;
+    }
+
     public void DriveCarOnTruck(Car car){
-        if (rampUp == false && (CarsOnTruck.size() < maxCarsOnTruck)){
+        if (!rampUp && (CarsOnTruck.size() < maxCarsOnTruck) && !car.isCarOnTruck() && CanCarDriveOn(car)){
             CarsOnTruck.add(car);
             car.GetCarOnTruck();
         }
         else if(rampUp){
-            throw new ArithmeticException("Platform has to be down for cars to drive on");
+            throw new ArithmeticException("Platform has to be down for cars to drive on");  
         }
-        else{
+        else if(CarsOnTruck.size() == 5){
             throw new ArithmeticException("Car transporter already full");
         }
+        else if(!CanCarDriveOn(car)){
+            throw new ArithmeticException("Car is too far away");
+        }
+        }
 
-    }
+    
 
     public void RemoveCarFromTruck(Car car){
-        if (rampUp == false && CarsOnTruck.size() > 0 && car.isCarOnTruck()){
+        if (!rampUp && CarsOnTruck.size() > 0 && car.isCarOnTruck()){
             CarsOnTruck.remove(CarsOnTruck.indexOf(car));
             car.GetCarOffTruck();
         }
@@ -55,11 +64,37 @@ public class CarTransporter extends Truck {
         }
     }
 
+    private boolean CanCarDriveOn(Car car){
+        int xDiff = this.getX() - car.getX();
+        int yDiff = this.getY() - car.getY();
+        return Math.sqrt(Math.pow(xDiff, 2) + (Math.pow(yDiff, 2))) < minDistanceToDriveOnTruck;
+    }
+
+    private void MatchCarsAndTransportSpeed(){
+        for (Car car : CarsOnTruck){
+            car.setCurrentSpeed(this.getCurrentSpeed());
+        }
+    }
+
+    private void MoveCarInSameDirection(){
+        for (Car car : CarsOnTruck){
+            if(car.getDirection() == this.getDirection()){
+                car.move();
+            }
+            else{
+                car.turnLeft();
+            }
+        }
+    }
+
 
     @Override
     public void move(){
         if (rampUp){
             super.move();
+            MatchCarsAndTransportSpeed();
+            MoveCarInSameDirection();
+            
         }
         else{
             throw new ArithmeticException("Can't drive with the ramp down!");
---
+++ test/set/CarTransporterTest.java
@@ -0,0 +1,137 @@
+package set;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CarTransporterTest {
+    @Test
+    public void Ramp_can_be_down_if_CarTransporter_is_stationairy(){
+        CarTransporter carTransporter = new CarTransporter();
+        
+        carTransporter.LowerRamp();
+
+        assertFalse(carTransporter.isRampUp());
+    }
+
+    @Test
+    public void CarTransporter_can_not_move_when_ramp_is_down(){
+        CarTransporter carTransporter = new CarTransporter();
+        
+        carTransporter.LowerRamp();
+
+        assertThrows(ArithmeticException.class, () ->{
+            carTransporter.move();
+        });
+    }
+
+    @Test
+    public void Ramp_can_not_be_lowered_when_moving(){
+        CarTransporter carTransporter = new CarTransporter();
+
+        carTransporter.setCurrentSpeed(5);
+
+        assertThrows(ArithmeticException.class, () ->{
+            carTransporter.LowerRamp();
+        });
+    }
+
+    @Test
+    public void Car_can_not_drive_on_if_ramp_is_up(){
+        CarTransporter carTransporter = new CarTransporter();
+        Volvo240 volvo = new Volvo240();
+        
+        
+        assertThrows(ArithmeticException.class, () ->{
+            carTransporter.DriveCarOnTruck(volvo);
+        });
+    }
+
+    @Test
+    public void Car_can_not_drive_on_if_CarTransporter_is_full(){
+        CarTransporter carTransporter = new CarTransporter();
+        Volvo240 volvo = new Volvo240();
+        Volvo240 volvo1 = new Volvo240();
+        Volvo240 volvo2 = new Volvo240();
+        Volvo240 volvo3 = new Volvo240();
+        Volvo240 volvo4 = new Volvo240();
+        Volvo240 volvo5 = new Volvo240();
+
+        carTransporter.LowerRamp();
+
+        carTransporter.DriveCarOnTruck(volvo);
+        carTransporter.DriveCarOnTruck(volvo1);
+        carTransporter.DriveCarOnTruck(volvo2);
+        carTransporter.DriveCarOnTruck(volvo3);
+        carTransporter.DriveCarOnTruck(volvo4);
+
+        assertThrows(ArithmeticException.class, () ->{
+            carTransporter.DriveCarOnTruck(volvo5);
+        });
+    }
+
+    @Test
+    public void Car_can_not_drive_off_if_ramp_is_up(){
+        CarTransporter carTransporter = new CarTransporter();
+        Volvo240 volvo = new Volvo240();
+
+        carTransporter.LowerRamp();
+
+        carTransporter.DriveCarOnTruck(volvo);
+
+        carTransporter.LiftRamp();
+        
+        
+        assertThrows(ArithmeticException.class, () ->{
+            carTransporter.RemoveCarFromTruck(volvo);
+        });
+    }
+
+    @Test
+    public void Car_can_not_drive_off_if_ramp_is_emoty(){
+        CarTransporter carTransporter = new CarTransporter();
+        Volvo240 volvo = new Volvo240();
+        
+        
+        assertThrows(ArithmeticException.class, () ->{
+            carTransporter.RemoveCarFromTruck(volvo);
+        });
+    }
+
+    @Test
+    public void Car_can_not_drive_on_if_far_away(){
+        CarTransporter carTransporter = new CarTransporter();
+        Volvo240 volvo = new Volvo240();
+
+        volvo.setCurrentSpeed(50);
+        volvo.move();
+
+        carTransporter.LowerRamp();
+
+        assertThrows(ArithmeticException.class, () ->{
+            carTransporter.DriveCarOnTruck(volvo);
+        });
+    }
+
+    @Test
+    public void Car_and_CarTransporter_should_have_the_same_coords_when_Car_is_on(){
+        CarTransporter carTransporter = new CarTransporter();
+        Volvo240 volvo = new Volvo240();
+
+        carTransporter.LowerRamp();
+
+        carTransporter.DriveCarOnTruck(volvo);
+
+        carTransporter.LiftRamp();
+
+        carTransporter.startVehicle();
+        carTransporter.gas(0.5);
+        carTransporter.move();
+        carTransporter.turnLeft();
+        carTransporter.move();
+        carTransporter.turnLeft();
+
+        assertTrue(volvo.getX() == carTransporter.getX() && volvo.getY() == carTransporter.getY());
+    }
+
+
+}
